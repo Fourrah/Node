5a63d815d6031666c66f5a7b5cfbf49e6a2eb0d7
Sara_Jeon <saraj@tecace.com>
2023-05-02 16:32:51 +0900

    Classify Files

--- app.js
+++ app.js
@@ -1,38 +1,81 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const directoryPath = '/Users/Sara/myStudyRoom/node/chapter7/pictures/test';
+const directoryPath = "/Users/Sara/myStudyRoom/node/chapter7/pictures/test";
 
-if(!directoryPath) {
-    console.error;
+if (!directoryPath) {
+  console.error;
 }
 
 fs.readdir(directoryPath, function (err, files) {
-    if(err) {
-        console.error;
-    }
+  if (err) {
+    console.error;
+  }
 
-    files.forEach((file) => {
-        console.log(file);
-    })
+  folderAdd("video", files);
+  folderAdd("captured", files);
+  folderAdd("duplicated", files);
 
-    folderAdd('video', files);
-    folderAdd('captured', files);
-    folderAdd('duplicated', files);
+  moveFile(files);
 });
 
 const folderAdd = (folderName, files) => {
-    if(!files.includes(folderName)) {
-        fs.mkdir(path.join(directoryPath, folderName), (err) => {
+  if (!files.includes(folderName)) {
+    fs.mkdir(path.join(directoryPath, folderName), (err) => {
+      if (err) {
+        console.error;
+        return;
+      }
+      console.log(`${folderName} 폴더가 생성되었습니다.`);
+    });
+  } else {
+    console.log(`${folderName} 폴더가 이미 존재합니다.`);
+  }
+};
+
+const moveFile = (files) => {
+  files.forEach((file) => {
+    let temp = file.split(".");
+    if (temp[1] === "mov" || temp[1] === "mp4") {
+      fs.rename(
+        path.join(directoryPath, file),
+        path.join(directoryPath, "video", file),
+        (err) => {
+          if (err) {
+            console.error;
+          }
+          console.log(`${file} 파일을 video 폴더로 이동했습니다.`);
+        }
+      );
+    } else if (temp[1] === 'png' || temp[1] === 'aae') {
+        fs.rename(
+            path.join(directoryPath, file),
+            path.join(directoryPath, "captured", file),
+            (err) => {
+              if (err) {
+                console.error;
+              }
+              console.log(`${file} 파일을 captured 폴더로 이동했습니다.`);
+            }
+        );
+    } else {
+        fs.stat(path.join(directoryPath,file), (err, stats) => {
             if(err) {
                 console.error;
-                return;
-            } 
-            console.log(`${folderName} 폴더가 생성되었습니다.`);
+            }
+            if(stats.isFile() && !(file.includes("_E"))) {
+                fs.rename(
+                    path.join(directoryPath, file),
+                    path.join(directoryPath, "duplicated", file),
+                    (err) => {
+                        if (err) {
+                        console.error;
+                        }
+                        console.log(`${file} 파일을 duplicated 폴더로 이동했습니다.`);
+                    }
+                );
+            }
         });
-    } else {
-        console.log(`${folderName} 폴더가 이미 존재합니다.`);
     }
-}
-
-
+  });
+};
