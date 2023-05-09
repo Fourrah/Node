a0871402b08b8e0b414f9db492e3a10dc93ccdcf
Sara_Jeon <saraj@tecace.com>
2023-05-09 15:48:03 +0900

    전체 트윗 또는 사용자 이름에 맞는 트윗 보여주기

--- dwitter/server/router/tweetsRouter.js
+++ dwitter/server/router/tweetsRouter.js
@@ -3,10 +3,43 @@ import 'express-async-errors';
 
 const router = express.Router();
 
+
+let tweets = [
+    {
+        id: '1',
+        text: '노드 공부하기',
+        createdAt: Date.now().toString(),
+        name: 'Sara',
+        userName: 'sara'
+    },
+    {
+        id: '2',
+        text: '코테 공부하기',
+        createdAt: Date.now().toString(),
+        name: 'Will',
+        userName: 'will'
+    },
+    {
+        id: '3',
+        text: '일하기',
+        createdAt: Date.now().toString(),
+        name: 'Loopy',
+        userName: 'loopy'
+    },
+    
+]
+
 // Get /tweets
-// Get / tweets/:user_id
+// Get / tweets?userName=userName
+router.get('/', (req, res, next) => {
+    const userName = req.params.userName;
+    tweets = userName ? tweets.filter((tweet) => { tweet.userName === userName}) : tweets;
+    res.status(200).json(tweets);
+    
+})
+
 // Post /tweets
 // Put /tweets/:tweet_id
 // Delete /tweets/:tweet_id
 
-module.exports = router;
+export default router;
